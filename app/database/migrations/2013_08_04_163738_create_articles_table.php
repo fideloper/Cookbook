bb353a8ab215f48a81718f5064f55ab41f8abb18
Fideloper <fideloper@gmail.com>
2013-08-04 16:44:13 +0000

    added migrations and db config for articles, tags, articles_tags and users

---
+++ app/database/migrations/2013_08_04_163738_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->string('slug')->index();
+			$table->text('content');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('articles');
+	}
+
+}
